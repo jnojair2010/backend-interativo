97abe4bb65ff2692bf21b2b67bd82e2ac9637a3b
jair do Nascimento de Oliveira <jnojair2010@gmail.com>
2025-01-26 17:26:05 -0300

    created projeto backend interativo pdv

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const projetoBackEnd: {
+    professor:string,
+    escola:string
+} = {
+    professor : "jair de Oliveira de Oliveira",
+    escola : " Mauricio de Nassau"
+}
+
+console.log(projetoBackEnd)
